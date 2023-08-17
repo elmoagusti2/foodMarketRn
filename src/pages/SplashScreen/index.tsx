6c0d380650234d9ad1cb2afd0e77a6101f046575
elmoagusti2 <elmo.agusti@i-solution.co.id>
2023-08-17 22:40:15 +0700

    chores: setup RNsvg and adding splash screen

---
+++ src/pages/SplashScreen/index.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { SafeAreaView, Text, View } from 'react-native';
+import { Logo } from '../../assets';
+function SplashScreen(): JSX.Element {
+    return (
+        <SafeAreaView
+            style={{
+                backgroundColor: '#FFC700',
+                flex: 1,
+                justifyContent: 'center',
+                alignItems: 'center'
+            }}>
+            <Logo />
+            <View style={{ height: 38 }} />
+            <Text style={{ fontSize: 32, color: '#020202' }} >FoodMarket</Text>
+        </SafeAreaView>
+    );
+}
+export default SplashScreen;
+
