ea1b29ea0a0d1747ca21c81c6625b9b3074c4ec4
elmoagusti2 <yoshimitsusanandreo@ymail.com>
2024-03-01 11:23:36 +0700

    updates: slicing user section in home

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,14 +1,40 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { Image, StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import { SafeAreaView } from 'react-native-safe-area-context'
 
 const Home = () => {
   return (
-    <View>
-      <Text>Home</Text>
-    </View>
+    <SafeAreaView>
+      <View style={styles.userSection}>
+        <View>
+          <Text style={styles.title}>FoodMarket</Text>
+          <Text style={styles.subTitle} >Lets get some foods</Text>
+        </View>
+        <Image source={{ uri: 'https://cdn4.iconfinder.com/data/icons/avatars-xmas-giveaway/128/batman_hero_avatar_comics-512.png' }}
+          style={{ width: 70, height: 70, borderRadius: 20 }} />
+      </View>
+    </SafeAreaView>
   )
 }
 
 export default Home
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  userSection: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    paddingHorizontal: 20,
+    paddingVertical: 20,
+    backgroundColor: 'white'
+  },
+  title: {
+    fontFamily: 'Poppins-Regular',
+    fontSize: 22,
+    color: '#020202'
+  },
+  subTitle: {
+    fontFamily: 'Poppins-Regular',
+    fontSize: 14,
+  },
+})
