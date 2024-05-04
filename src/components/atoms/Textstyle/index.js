d7ab32fb11549d896dc6e87685ab7d6d1299d0e5
elmoagusti2 <yoshimitsusanandreo@ymail.com>
2024-05-04 09:17:20 +0700

    chores: adding textstyle reusable

---
+++ src/components/atoms/Textstyle/index.js
@@ -0,0 +1,134 @@
+import { StyleSheet } from 'react-native'
+
+
+const TextStyle = StyleSheet.create({
+    regular4: {
+        fontSize: 4,
+        fontFamily: 'Poppins-Regular',
+    },
+    bold4: {
+        fontSize: 4,
+        fontWeight: 'bold',
+        fontFamily: 'Poppins-Regular',
+    },
+    regular6: {
+        fontSize: 6,
+        fontFamily: 'Poppins-Regular',
+    },
+    bold6: {
+        fontSize: 6,
+        fontWeight: 'bold',
+        fontFamily: 'Poppins-Regular',
+    },
+    regular8: {
+        fontSize: 8,
+        fontFamily: 'Poppins-Regular',
+    },
+    bold8: {
+        fontSize: 8,
+        fontWeight: 'bold',
+        fontFamily: 'Poppins-Regular',
+    },
+    regular10: {
+        fontSize: 10,
+        fontFamily: 'Poppins-Regular',
+    },
+    bold10: {
+        fontSize: 10,
+        fontWeight: 'bold',
+        fontFamily: 'Poppins-Regular',
+    },
+    regular12: {
+        fontSize: 12,
+        fontFamily: 'Poppins-Regular',
+    },
+    bold12: {
+        fontSize: 12,
+        fontWeight: 'bold',
+        fontFamily: 'Poppins-Regular',
+    },
+    regular14: {
+        fontSize: 14,
+        fontFamily: 'Poppins-Regular',
+    },
+    bold14: {
+        fontSize: 14,
+        fontWeight: 'bold',
+        fontFamily: 'Poppins-Regular',
+    },
+    regular16: {
+        fontSize: 16,
+        fontFamily: 'Poppins-Regular',
+    },
+    bold16: {
+        fontSize: 16,
+        fontWeight: 'bold',
+        fontFamily: 'Poppins-Regular',
+    },
+    regular18: {
+        fontSize: 18,
+        fontFamily: 'Poppins-Regular',
+    },
+    bold18: {
+        fontSize: 18,
+        fontWeight: 'bold',
+        fontFamily: 'Poppins-Regular',
+    },
+    regular20: {
+        fontSize: 20,
+        fontFamily: 'Poppins-Regular',
+    },
+    bold20: {
+        fontSize: 20,
+        fontWeight: 'bold',
+        fontFamily: 'Poppins-Regular',
+    },
+    regular22: {
+        fontSize: 22,
+        fontFamily: 'Poppins-Regular',
+    },
+    bold22: {
+        fontSize: 22,
+        fontWeight: 'bold',
+        fontFamily: 'Poppins-Regular',
+    },
+    regular24: {
+        fontSize: 24,
+        fontFamily: 'Poppins-Regular',
+    },
+    bold24: {
+        fontSize: 24,
+        fontWeight: 'bold',
+        fontFamily: 'Poppins-Regular',
+    },
+    regular26: {
+        fontSize: 26,
+        fontFamily: 'Poppins-Regular',
+    },
+    bold26: {
+        fontSize: 26,
+        fontWeight: 'bold',
+        fontFamily: 'Poppins-Regular',
+    },
+    regular28: {
+        fontSize: 28,
+        fontFamily: 'Poppins-Regular',
+    },
+    bold28: {
+        fontSize: 28,
+        fontWeight: 'bold',
+        fontFamily: 'Poppins-Regular',
+    },
+    regular30: {
+        fontSize: 30,
+        fontFamily: 'Poppins-Regular',
+    },
+    bold30: {
+        fontSize: 30,
+        fontWeight: 'bold',
+        fontFamily: 'Poppins-Regular',
+    },
+
+})
+
+export default TextStyle;
--- src/components/atoms/index.js
+++ src/components/atoms/index.js
@@ -2,10 +2,11 @@ import TextInput from "./TextInput";
 import Buttons from "./Buttons";
 import Gap from "./Gap";
 import Select from "./Select";
-
+import TextStyle from "./Textstyle";
 export {
     TextInput,
     Buttons,
     Gap,
     Select,
+    TextStyle,
 };
