c2fc66c429e0daa41e61a385995a34eeb2f15731
elmoagusti2 <yoshimitsusanandreo@ymail.com>
2024-12-04 14:00:35 +0700

    chores: authenticated splashScreen

--- src/pages/SplashScreen/index.js
+++ src/pages/SplashScreen/index.js
@@ -1,12 +1,28 @@
 import { SafeAreaView, Text, View, useColorScheme } from "react-native";
 import { Store } from "../../assets";
 import { useEffect } from "react";
+import { getString } from "../../utils";
+import { showFlashMessage } from "../../components";
 
 const SplashScreen = ({ navigation }) => {
     useEffect(() => {
-        setTimeout(() => {
-            navigation.replace('SignIn')
-        }, 2000);
+        try {
+            const token = getString('token');
+            if (token) {
+                setTimeout(() => {
+                    navigation.replace('Main');
+                }, 1000);
+            } else {
+                setTimeout(() => {
+                    navigation.replace('SignIn');
+                }, 1000);
+            }
+        } catch (err) {
+            showFlashMessage(err.message, 'ERROR');
+            setTimeout(() => {
+                navigation.replace('SignIn');
+            }, 0);
+        }
     });
 
     return <SafeAreaView style={{
