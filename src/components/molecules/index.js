b96871c551821d2009332158ddce4cbf17aa48ac
elmoagusti2 <yoshimitsusanandreo@ymail.com>
2024-12-05 15:19:36 +0700

    chores: integration logout in profile

--- src/components/molecules/index.js
+++ src/components/molecules/index.js
@@ -2,9 +2,13 @@ import Header from "./Header";
 import FoodCard from "./FoodCard";
 import FoodList from "./FoodList";
 import Loading from "./Loading";
+import TabOrders from "./TabOrders";
+import TabProfiles from "./TabProfiles";
 export {
     Header,
     FoodCard,
     FoodList,
     Loading,
+    TabOrders,
+    TabProfiles,
 };
--- src/pages/Profile/components/account.js
+++ src/pages/Profile/components/account.js
@@ -2,6 +2,7 @@ import { StyleSheet } from 'react-native'
 import React from 'react'
 import { ScrollView } from 'react-native-gesture-handler'
 import ProfileButton from './profile_button'
+import { deleteString } from '../../../utils'
 
 const Account = ({ navigation }) => {
     return (
@@ -10,6 +11,10 @@ const Account = ({ navigation }) => {
             <ProfileButton onTap={null} title={"Home Address"} />
             <ProfileButton onTap={null} title={"Security"} />
             <ProfileButton onTap={() => navigation.navigate('OrderHistory')} title={"Order History"} />
+            <ProfileButton onTap={() => {
+                deleteString("token");
+                navigation.replace('SignIn');
+            }} title={"Sign Out"} />
         </ScrollView>
     )
 }
