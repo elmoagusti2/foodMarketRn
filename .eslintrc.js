a1ca3ee3ceef2de7c15e4178bc8946c019630d4f
elmoagusti2 <yoshimitsusanandreo@ymail.com>
2025-09-24 09:52:29 +0700

    chores: penyesuaian ios

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,34 @@
 module.exports = {
   root: true,
-  extends: '@react-native',
-};
+  env: {
+    browser: true,
+    es2021: true,
+    node: true
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:react-native/all',
+    'prettier'
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
+    },
+    ecmaVersion: 2021,
+    sourceType: 'module'
+  },
+  plugins: ['react', 'react-hooks', 'react-native'],
+  rules: {
+    'react/prop-types': 'off',
+    'react-native/no-inline-styles': 'off',
+    'react-native/split-platform-components': 'off',
+    'react-native/no-raw-text': 'off'
+  },
+  settings: {
+    react: {
+      version: 'detect'
+    }
+  }
+}
--- .prettierrc.js
+++ .prettierrc.js
@@ -1,7 +1,6 @@
 module.exports = {
-  arrowParens: 'avoid',
-  bracketSameLine: true,
-  bracketSpacing: false,
-  singleQuote: true,
-  trailingComma: 'all',
+  "singleQuote": true,       // Pakai tanda kutip tunggal untuk string → 'hello' bukan "hello"
+  "semi": false,             // Hilangkan titik koma di akhir baris
+  "trailingComma": "es5",    // Tambah koma di akhir elemen hanya di array/object (bukan di fungsi)
+  "printWidth": 80           // Batas maksimal lebar baris → lebih dari 80 akan dibungkus
 };
